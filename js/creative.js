065d73e7200b68ff126ad1431962da989e331b2d
Vako Beridze <vako.beridze@gmail.com>
2016-04-22 18:05:11 +0400

    added map

--- js/creative.js
+++ js/creative.js
@@ -40,9 +40,30 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(28.1823294, -82.352912),
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        scrollwheel: false,
+        draggable: false,
+        panControl: true,
+        zoomControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        overviewMapControl: true,
+        rotateControl: true
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
